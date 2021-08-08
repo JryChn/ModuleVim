24db5c7a5fa06d6962ccbfb349270d13b8851a69
JryChn <jeremychen@djeremychen.com>
2021-08-08 23:22:42 +0800

    add go debug with delve

--- lua/languages/config/nvim-dap.lua
+++ lua/languages/config/nvim-dap.lua
@@ -89,22 +89,61 @@ require("dapui").setup({
 
 local dap = require "dap"
 
-dap.adapters.go = {
-	type = 'executable';
-	command = 'node';
-	args = {
-		vim.fn.stdpath("data") .. "/dapinstall/go_dbg/vscode-go/dist/debugAdapter.js"
-	};
-}
-dap.configurations.go = {
-	{
-		type = 'go';
-		name = 'Debug';
-		request = 'launch';
-		showLog = false;
-		program = "${file}";
-		dlvToolPath = vim.fn.exepath(vim.fn.stdpath("data") .. "/dapinstall/go_dbg/delve/cmd/dlv")
-
-		-- Adjust to where delve is installed
-	},
+dap.adapters.go = function(callback, config)
+    local stdout = vim.loop.new_pipe(false)
+    local handle
+    local pid_or_err
+    local port = 38697
+    local dlv_path = vim.fn.stdpath("data") .. "/dapinstall/go_delve_dbg/delve/cmd/dlv"
+    local opts = {
+      stdio = {nil, stdout},
+      args = {"dap", "-l", "127.0.0.1:" .. port},
+      detached = true
+    }
+    handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+      stdout:close()
+      handle:close()
+      if code ~= 0 then
+        print('dlv exited with code', code)
+      end
+    end)
+    assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
+    stdout:read_start(function(err, chunk)
+      assert(not err, err)
+      if chunk then
+        vim.schedule(function()
+          require('dap.repl').append(chunk)
+        end)
+      end
+    end)
+    -- Wait for delve to start
+    vim.defer_fn(
+      function()
+        callback({type = "server", host = "127.0.0.1", port = port})
+      end,
+      100)
+  end
+  -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+  dap.configurations.go = {
+    {
+      type = "go",
+      name = "Debug",
+      request = "launch",
+      program = "${file}",
+    },
+    {
+      type = "go",
+      name = "Debug test", -- configuration for debugging test files
+      request = "launch",
+      mode = "test",
+      program = "${file}",
+    },
+    -- works with go.mod packages and sub packages 
+    {
+      type = "go",
+      name = "Debug test (go.mod)",
+      request = "launch",
+      mode = "test",
+      program = "./${relativeFileDirname}",
+    } 
 }
