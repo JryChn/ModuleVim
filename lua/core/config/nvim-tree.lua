107c34234ad983ec93ab13bb5bdbbb3f750e03ec
JryChn <jrychn@djeremychen.com>
2021-09-26 14:23:18 +0800

    upstream update

--- lua/core/config/nvim-tree.lua
+++ lua/core/config/nvim-tree.lua
@@ -41,6 +41,54 @@ vim.api.nvim_set_keymap(
 	":NvimTreeToggle<CR>",
 	{noremap = true, silent = true}
 )
+require 'nvim-tree'.setup {
+	-- disables netrw completely
+	disable_netrw = true,
+
+	-- hijack netrw window on startup
+	hijack_netrw = true,
+
+	-- open the tree when running this setup function
+	open_on_setup = false,
+
+	-- will not open on setup if the filetype is in this list
+	ignore_ft_on_setup = {},
+
+	-- closes neovim automatically when the tree is the last **WINDOW** in the view
+	auto_close = true,
+
+	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+	open_on_tab = false,
+
+	-- hijack the cursor in the tree to put it at the start of the filename
+	hijack_cursor = false,
+
+	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+	update_cwd = false,
+
+	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+	update_focused_file = {
+		-- enables the feature
+		enable = false,
+
+		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+		-- only relevant when `update_focused_file.enable` is true
+		update_cwd = false,
+
+		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+		ignore_list = {}
+	},
+
+	-- configuration options for the system open command (`s` in the tree by default)
+	system_open = {
+		-- the command to run this, leaving nil should work in most cases
+		cmd = nil,
+
+		-- the command arguments as a list
+		args = {}
+	},
+}
 
 local g = vim.g
 
@@ -48,17 +96,7 @@ vim.o.termguicolors = true
 
 g.nvim_tree_side = "left"
 g.nvim_tree_width = 26
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 1
-g.nvim_tree_quit_on_open = 0
-g.nvim_tree_follow = 1
-g.nvim_tree_indent_markers = 1
-
---g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ":t"
-g.nvim_tree_tab_open = 0
-g.nvim_tree_allow_resize = 1
 
 g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
 
