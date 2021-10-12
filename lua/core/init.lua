2642b7200ef483e95c1ed9af6dae465558f7a2bd
JryChn <jeremychen@djeremychen.com>
2021-10-12 17:18:12 +0800

    fix telescope not used require

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -80,7 +80,6 @@ packer.startup(function(use)
 		requires = {
 			{'nvim-lua/popup.nvim'},
 			{'nvim-lua/plenary.nvim'},
-			{'BurntSushi/ripgrep'}
 		},
 		config = function()
 			require 'core.config.telescope'
