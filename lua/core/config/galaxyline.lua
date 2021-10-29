093eb45c5cdf88580ca2968ee60347d6c445a913
JryChn <jeremychen@djeremychen.com>
2021-10-29 16:55:08 +0800

    fix vista galaxyline

--- lua/core/config/galaxyline.lua
+++ lua/core/config/galaxyline.lua
@@ -2,7 +2,7 @@ local gl = require("galaxyline")
 local gls = gl.section
 require 'nvim-gps'.setup()
 
-gl.short_line_list = {"NvimTree", "vista_kind", "toggleterm"}
+gl.short_line_list = {"NvimTree", "vista_kind", "toggleterm","vista_markdown","vista"}
 
 local colors = {
 	bg = "#1e222a",
